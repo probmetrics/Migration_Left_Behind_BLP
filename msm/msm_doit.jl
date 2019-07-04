9fea6de7992fdce9da820bf1e1c3e97b0055e208
probmetrics <stan.sugar.xu@gmail.com>
2019-07-04 16:43:36 +0800

    first version of msm moment conditions

---
+++ msm/msm_doit.jl
@@ -0,0 +1,136 @@
+using FileIO, DataFrames, CSV, Statistics, StatsBase, LinearAlgebra, GLM
+DTDIR = "E:/NutStore/Research/mig_leftbh_enrollment"
+WKDIR = "E:/Dropbox/GitHub/Migration_Left_Behind_BLP"
+
+include("$WKDIR/utility_funs/obj_helper_funs.jl")
+include("$WKDIR/msm/data_prepare_msm.jl")
+# include("msl_llk.jl")
+# include("$WKDIR/msl/msl_llk_loop.jl")
+# include("$WKDIR/utility_funs/squarem_helper_funs.jl")
+# include("$WKDIR/utility_funs/blp_squarem.jl")
+# include("$WKDIR/msl/msl_est_iter.jl")
+
+
+##
+## 1. load choice data from MPS
+##
+
+LeftbhData = CSV.read("$DTDIR/mig_leftbh_enroll_fit.csv"; type = Float64)
+LeftbhData[:cagey] = LeftbhData[:cagey] / 10
+LeftbhData[:cageysq] = LeftbhData[:cagey].^2
+LeftbhData[:nchild_lnmw] = LeftbhData[:lnmnw_city].* LeftbhData[:nchild]
+LeftbhData[:nchild_lnhp] = LeftbhData[:lnhprice].* LeftbhData[:nchild]
+LeftbhData[:hhtype] = LeftbhData[:hhtype] .- 1
+YL = Vector{Float64}(LeftbhData[:chosen])
+# YM = Vector{Float64}(LeftbhData[:child_leftbh])
+
+lnDataShare, Delta_init, lnW, lnP, lnQX, wgt, sgwgt, XT, XM, XL, XF, XQ, pr_lft,
+	pr_lft_alt, nalt, nind, dgvec, htvec = data_prepare(LeftbhData; trs = true)
+
+##
+## 2. get random draw
+##
+
+MigBootData = CSV.read("$DTDIR/mig_leftbh_indboot.csv")
+
+# --- bootstrap random shock ---
+nsim = 10
+ndraw = nind * nsim
+alpha = 0.12
+
+# bootstrap observed preference vars.
+DF_master = LeftbhData[LeftbhData[:chosen] .== 1,
+            [:year, :ID, :cline, :child_leftbh,
+            :highsch_f, :highsch_m]]
+rename!(DF_master, :child_leftbh => :leftbh)
+zshk_vars = [:caring_study, :college_expect]
+match_vars = [:leftbh, :highsch_f, :highsch_m]
+
+Random.seed!(20190610);
+ZSHK = map(1:nrow(DF_master)) do i
+    bdf = filter(df -> df[match_vars] == DF_master[i, match_vars], MigBootData)
+    Matrix{Float64}(boot_df(bdf, zshk_vars; nboot = nsim))
+end
+ZSHK = vcat(ZSHK...)
+ZSHK = copy(ZSHK')
+
+USHK = draw_shock(ndraw; dims = 2) # draw iid standard normal random shock
+QSHK = USHK[:, 2]
+USHK = USHK[:, 1]
+
+##
+## 3. search for initial values
+##
+
+XTnames = [:highsch_f, :highsch_m, :age_f, :age_m, :han]
+XFnames = [:treat, :migscore_fcvx_city, :lnhprice, :migscore_treat, :lnhp_treat,
+		   :lnmnw_city, :nchild_lnmw, :nchild_lnhp]
+XLnames = [:cfemale, :nchild, :cagey, :cageysq]
+lftvar = [XTnames; XFnames; XLnames]
+lft_form = @eval @formula(child_leftbh ~ $(Meta.parse(join(lftvar, " + "))))
+lft_fit = glm(lft_form, view(LeftbhData, YL .== 1, :),
+			  Binomial(), LogitLink(), wts = wgt)
+lft_init = coef(lft_fit)
+
+##
+## 4. Evaluate the likelihood
+##
+
+nparm = size(XT, 1) + size(XL, 1) + size(XM, 1) + size(XF, 1) + 3 +
+		size(XQ, 1) + size(ZSHK, 1) + 3
+xt_init = lft_init[1:6]
+xf_init = lft_init[7:14]
+xl_init = lft_init[15:end]
+xm_init = zeros(size(XM, 1))
+xq_init = zeros(size(XQ, 1))
+bw = 0.194
+blft = -0.148
+bitr = -0.167
+initval = [xt_init; 0.0; xl_init; xm_init; 0.0; xf_init; blft; bw; bitr;
+		   xq_init; zeros(2); -1.5; 0.0; -1.0]
+
+# --- iterative maximization ---
+# msl_est_iter(initval, lnDataShare, Delta_init, YL, YM, lnW, lnP, XT, XL, XM, XF,
+# 			 XQ, ZSt, USHK, wgt, sgwgt, nind, nalt, nsim, dgvec; biter = 3)
+
+# --- evaluate moment conditions ---
+@time get_moments(initval, alpha, lnW, lnP, lnQX, XT, XL, XM, XF, XQ, ZSHK,
+				  USHK, QSHK, pr_lft, pr_lft_alt, Delta_init, dgvec, htvec, wgt,
+				  sgwgt, nind, nalt, nsim; xdim = 1)
+
+# 430352.6129; 5.2s
+#
+# @time mig_leftbh_llk_thread(initval, Delta_init, YL, YM, lnW, lnP, XT, XL, XM, XF, XQ,
+# 			   ZSt, USHK, wgt, nind, nalt, nsim, dgvec, 0.12, 1)
+# # 430352.6129; 0.8s for 8 threads
+#
+# # --- test for ForwardDiff ---
+# using Optim, ForwardDiff
+# llk_opt = parm -> mig_leftbh_llk(parm, Delta_init, YL, YM, lnW, lnP, XT, XL, XM, XF,
+# 			   					 XQ, ZSt, USHK, wgt, nind, nalt, nsim, dgvec, 0.12, 1)
+# @time llk_opt(initval)
+# @time gr = ForwardDiff.gradient(llk_opt, initval)
+#
+# llk_opt_thread = parm -> mig_leftbh_llk_thread(parm, Delta_init, YL, YM, lnW, lnP,
+# 											   XT, XL, XM, XF, XQ, ZSt, USHK, wgt,
+# 											   nind, nalt, nsim, dgvec, 0.12, 1)
+# @time llk_opt_thread(initval)
+# @time gr = ForwardDiff.gradient(llk_opt_thread, initval)
+#
+# # --- predicted location choice probabilities ---
+# ngrp = length(sgwgt)
+# mktshare = zeros(nalt, ngrp)
+# @time locpr_serial!(mktshare, initval, Delta_init, lnW, lnP, XT, XL, XM,
+# 			   		XF, XQ, ZSt, USHK, wgt, sgwgt, nind, nalt, nsim, dgvec)
+# @time locpr_thread!(mktshare, initval, Delta_init, lnW, lnP, XT, XL, XM,
+# 			   		XF, XQ, ZSt, USHK, wgt, sgwgt, nind, nalt, nsim, dgvec)
+#
+# # --- BLP Contraction Mapping ---
+# delta_fpt = zeros(nalt, ngrp)
+# delta_new = zeros(nalt, ngrp)
+# delta_old = copy(Delta_init)
+# delta_q1 = zeros(nalt, ngrp)
+# delta_q2 = zeros(nalt, ngrp)
+# @time fpt_squarem!(delta_fpt, delta_new, delta_old, delta_q1, delta_q2, lnDataShare,
+# 				   initval, lnW, lnP, XT, XL, XM, XF, XQ, ZSt, USHK, wgt, sgwgt,
+# 				   nind, nalt, nsim, dgvec)
