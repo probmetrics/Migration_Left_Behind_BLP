6d4f5b13b09835d31aeef61f38d828bf5d09cd72
probmetrics <stan.sugar.xu@gmail.com>
2019-09-18 11:32:02 +0800

    add lnW in lambda, correct data preparing proc in msl_indept

--- msl_indept/data_prepare_msl.jl
+++ msl_indept/data_prepare_msl.jl
@@ -1,5 +1,8 @@
 using DataFrames
-function data_prepare(df::AbstractDataFrame; trs::Bool = false)
+function data_prepare(df::AbstractDataFrame, XQJMnames::AbstractVector{Symbol},
+                      XTnames::AbstractVector{Symbol}, XLnames::AbstractVector{Symbol},
+                      XFnames::AbstractVector{Symbol}, XMnames::AbstractVector{Symbol},
+                      XQnames::AbstractVector{Symbol}; trs::Bool = false)
     ndt = nrow(df)
     city_alts = Vector{Int64}(df[:, :city_alts])
     nalt = length(unique(city_alts))
@@ -9,11 +12,13 @@ function data_prepare(df::AbstractDataFrame; trs::Bool = false)
     dgvec = [locate_gidx(i, ngvec) for i = 1:nind]
 
     lnW = Vector{Float64}(df[:, :lnhinc_alts])
-    lnW = lnW .- mean(lnW, weights(Vector{Float64}(df[:, :w_l])))
+    wgtvec = Vector{Float64}(df[:w_l])
+    cage9d = Vector{Int}(df[:cage9])
+    lnW = lnW .- mean(view(lnW, cage9d .== 1), weights(view(wgtvec, cage9d .== 1)))
+
     lnP = Vector{Float64}(df[:, :lnhprice])
 
     wgt = Vector{Float64}(df[df[:, :chosen] .== 1, :w_l])
-
     sgwgt = countmap(dgvec, weights(wgt))
     sgwgt = [sgwgt[i] for i = 1:length(sgwgt)]
 
@@ -25,35 +30,26 @@ function data_prepare(df::AbstractDataFrame; trs::Bool = false)
     Delta_init = lnDataShare .- lnDataShare[1, :]'
 
     # --- household preference ---
-    XTnames = [:highsch_f, :highsch_m, :age_f, :age_m, :han]
     XT = [ones(nind) convert(Array{Float64, 2}, df[df[:, :chosen] .== 1, XTnames])]
 
     # --- migration cost ---
-    XMnames = [:lndist, :cross_prov, :cross_regn, :lndist_crsprov, :lndist_crsregn,
-               :amenity_pca_flowdur, :migdist_flowdur, :amenity_pca_highsch_f,
-               :migdist_highsch_f, :amenity_pca_highsch_m, :migdist_highsch_m,
-               :amenity_pca_age_f, :migdist_age_f, :amenity_pca_age_m, :migdist_age_m]
     XM = convert(Array{Float64, 2}, df[:, XMnames])
 
     # --- left-behind utility loss ---
-    XLnames = [:cfemale, :nchild, :cagey, :cageysq]
     XL = convert(Array{Float64, 2}, df[df[:, :chosen] .== 1, XLnames])
 
     # --- fixed cost ---
     # NOTE: the most critical part of the model!!
-    XFnames = [:htreat, :migscore_fcvx_city, :lnhprice, :migscore_treat, :lnhp_treat,
-               :lnmnw_city, :nchild_lnhp]
-    XF = [ones(ndt) convert(Array{Float64, 2}, df[:, XFnames])]
+    XF = [ones(ndt) convert(Array{Float64, 2}, df[:, XFnames]) lnW]
 
     # --- cognitive ability ---
     # TODO: how to incorporate regional eduation quality?
     #       we have identification problem.
 
-    XQnames = [:cfemale, :cagey, :nchild]
-    XQ = convert(Array{Float64, 2}, df[df[:, :chosen] .== 1, XQnames])
+    XQ = [ones(nind) convert(Array{Float64, 2}, df[df[:, :chosen] .== 1, XQnames])]
 
-    XQJnames = [:tstu2_ratio, :sschool_per]
-    XQJ_mig = convert(Array{Float64, 2}, df[:, XQJnames])
+    XQJ_mig = convert(Array{Float64, 2}, df[:, XQJMnames])
+	XQJ_mig = XQJ_mig .- mean(view(XQJ_mig, cage9d .== 1, :), weights(view(wgtvec, cage9d .== 1)), dims = 1)
 
     if trs == true
         XT = copy(XT')
--- msl_indept/msl_llk_doit.jl
+++ msl_indept/msl_llk_doit.jl
@@ -20,14 +20,27 @@ include("$WKDIR/msl_est_iter.jl")
 
 LeftbhData = CSV.read("$DTDIR/mig_leftbh_enroll_fit.csv"; type = Float64)
 # LeftbhData = sort(LeftbhData, (:year, :hhtype, :ID, :cline, :city_alts))
+LeftbhData[:cage9] = Float64.(LeftbhData[:cagey] .> 8)
 LeftbhData[:cagey] = LeftbhData[:, :cagey] / 10
 LeftbhData[:cageysq] = LeftbhData[:, :cagey].^2
 LeftbhData[:nchild_lnhp] = LeftbhData[:, :lnhprice].* LeftbhData[:, :nchild]
 YL = Vector{Float64}(LeftbhData[:, :chosen])
 YM = Vector{Float64}(LeftbhData[:, :child_leftbh])
 
+XMnames = [:lndist, :cross_prov, :cross_regn, :lndist_crsprov, :lndist_crsregn,
+           :amenity_pca_flowdur, :migdist_flowdur, :amenity_pca_highsch_f,
+           :migdist_highsch_f, :amenity_pca_highsch_m, :migdist_highsch_m,
+           :amenity_pca_age_f, :migdist_age_f, :amenity_pca_age_m, :migdist_age_m]
+XTnames = [:highsch_f, :highsch_m, :age_f, :han]
+XFnames = [:htreat, :migscore_fcvx_city, :lnhprice, :migscore_treat, :lnhp_treat,
+		   :nchild_lnhp]
+XLnames = [:cfemale, :nchild, :cagey, :cageysq, :age_m]
+XQnames = [:cfemale, :cagey, :nchild, :highsch_f, :highsch_m, :age_f]
+XQJnames = [:tstu2_ratio, :sschool_per]
+
 lnDataShare, Delta_init, lnW, lnP, XQJ_mig, wgt, sgwgt,
-XT, XM, XL, XF, XQ, nalt, nind, dgvec = data_prepare(LeftbhData; trs = true)
+XT, XM, XL, XF, XQ, nalt, nind, dgvec = data_prepare(LeftbhData, XQJnames,
+			 XTnames, XLnames, XFnames, XMnames, XQnames; trs = true)
 
 ##
 ## 2. get random draw
@@ -41,42 +54,25 @@ ndraw = nind * nsim
 alpha = 0.12
 
 # bootstrap observed preference vars.
-DF_master = LeftbhData[LeftbhData[:, :chosen] .== 1,
-            [:year, :ID, :cline, :child_leftbh,
-            :highsch_f, :highsch_m]]
-rename!(DF_master, :child_leftbh => :leftbh)
-Znames = [:caring_study, :college_expect]
-match_vars = [:leftbh, :highsch_f]
-
-Random.seed!(20190610);
-ZSHK = map(1:nrow(DF_master)) do i
-    bdf = filter(df -> df[match_vars] == DF_master[i, match_vars], MigBootData)
-    Matrix{Float64}(boot_df(bdf, Znames; nboot = nsim))
-end
-ZSHK = vcat(ZSHK...)
-ZSHK = copy(ZSHK')
 USHK = dropdims(draw_shock(ndraw; dims = 1); dims = 2) # draw iid standard normal random shock
 
 ##
 ## 3. search for initial values
 ##
 
-XTnames = [:highsch_f, :highsch_m, :age_f, :han]
-XLnames = [:nchild, :age_m]
-XMnames = [:lndist, :cross_prov, :cross_regn, :lndist_crsprov, :lndist_crsregn,
-		   :amenity_pca_flowdur, :migdist_flowdur, :amenity_pca_highsch_f,
-		   :migdist_highsch_f, :amenity_pca_highsch_m, :migdist_highsch_m,
-		   :amenity_pca_age_f, :migdist_age_f, :amenity_pca_age_m, :migdist_age_m]
-XFnames = [:htreat, :migscore_fcvx_city, :lnhprice, :migscore_treat, :lnhp_treat,
-		   :nchild_lnhp]
-XQnames = [:cfemale, :cagey]
-XQJnames = [:tstu2_ratio, :sschool_per]
-
 lftvar = [XTnames; XFnames; XLnames; XQJnames]
 lft_form = @eval @formula(child_leftbh ~ $(Meta.parse(join(lftvar, " + "))) + lnhinc_alts)
 lft_fit = glm(lft_form, view(LeftbhData, YL .== 1, :),
 			  Binomial(), LogitLink(), wts = wgt)
 lft_init = coef(lft_fit)
+
+lnq_form = @eval @formula(cog_adj ~ leftbh + clnhinc + $(Meta.parse(join(XQnames, " + "))) +
+						  tstu2_ratio + sschool_per + leftbh&clnhinc + leftbh&tstu2_ratio +
+						  leftbh&sschool_per)
+lnq_fit = lm(lnq_form, MigBootData)
+lnq_init = coef(lnq_fit)
+
+XbQ = XQ' * [lnq_init[1]; lnq_init[4:end-5]]
 
 ##
 ## 4. Evaluate the likelihood
@@ -86,37 +82,40 @@ initset = load("$DTDIR/msl_indept_results/msl_indept_est_20190807.jld2")
 initpar = initset["coefx"]
 initdel = initset["deltas"]
 xm_init = initpar[11:25]
-xf_init = [initpar[26:31]; initpar[33]]
+xf_init = [initpar[26:31]; initpar[33]; -0.4]
 
-nparm = size(XT, 1) + size(XL, 1) + size(XM, 1) + size(XF, 1) + 3 +
-		size(XQ, 1) + 2*size(XQJ_mig, 1) + size(ZSHK, 1) + 1
+nparm = size(XT, 1) + size(XL, 1) + size(XM, 1) + size(XF, 1)  +
+		2*size(XQJ_mig, 1) + 1
 
 xt_init = [3.0; lft_init[2:size(XT, 1)]]
-xl_init = lft_init[(size(XT, 1) + size(XF, 1)):(size(XT, 1) + size(XF, 1) + size(XL, 1) - 1)]
-xq_init = zeros(size(XQ, 1))
+xl_init = lft_init[(size(XT, 1) + size(XF, 1) - 1):(size(XT, 1) + size(XF, 1) + size(XL, 1) - 2)]
 xqj_init = lft_init[(end-size(XQJ_mig, 1)):end-1]
 
 bw = 0.194
 blft = -0.148
 bitr = -0.167
 
-initval = [xt_init; xl_init; xm_init; xf_init; blft; bw; bitr;
-		   xq_init; zeros(length(xqj_init)); xqj_init; zeros(2); -2]
+initval = [xt_init; xl_init; xm_init; xf_init; zeros(length(xqj_init)); xqj_init; -1.0]
+
+parm_names = [:XT_cons; Symbol.("XT_" .* string.(XTnames)); Symbol.("XL_" .* string.(XLnames));
+			  Symbol.("XM_" .* string.(XMnames)); :XF_cons; Symbol.("XF_" .* string.(XFnames));
+			  :XF_lnW; Symbol.("XQJ_Mig_" .* string.(XQJnames));
+			  Symbol.("XQJ_Dif_" .* string.(XQJnames)); :lnsigu]
 
 # --- iterative maximization ---
-ret_msl = msl_est_iter(initval, lnDataShare, Delta_init, YL, YM, lnW, lnP, XQJ_mig,
-			 			XT, XL, XM, XF, XQ, ZSHK, USHK, wgt, sgwgt, nind,
+ret_msl = msl_est_iter(initval, lnDataShare, initdel, YL, YM, lnW, lnP, XbQ,
+			 			XQJ_mig, XT, XL, XM, XF, USHK, wgt, sgwgt, nind,
 			 			nalt, nsim, dgvec; biter = 1)
 
 # --- evaluate log-likelihood ---
-@time mig_leftbh_llk(initval, Delta_init, YL, YM, lnW, lnP, XQJ_mig,
-			 		 XT, XL, XM, XF, XQ, ZSHK, USHK, wgt,
-			   		 nind, nalt, nsim, dgvec, 0.12, 1)
+@time mig_leftbh_llk(initval, initdel, YL, YM, lnW, lnP, XbQ, XQJ_mig,
+			 		 XT, XL, XM, XF, USHK, wgt, nind, nalt, nsim, dgvec,
+					 0.12, 1)
 # 444022.5091; 5.1s
 
-@time mig_leftbh_llk_thread(initval, Delta_init, YL, YM, lnW, lnP, XQJ_mig,
-			 		 		XT, XL, XM, XF, XQ, ZSHK, USHK, wgt,
-			   		 		nind, nalt, nsim, dgvec, 0.12, 1)
+@time mig_leftbh_llk_thread(initval, initdel, YL, YM, lnW, lnP, XbQ, XQJ_mig,
+			 		 		XT, XL, XM, XF, USHK, wgt, nind, nalt, nsim, dgvec,
+							0.12, 1)
 # 444022.5091; 0.9s for 8 threads
 #
 # --- test for ForwardDiff ---
