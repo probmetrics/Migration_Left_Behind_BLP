6fe911fcabb42e6ea0e9657a38f57061d3749f55
probmetrics <stan.sugar.xu@gmail.com>
2019-10-10 13:13:05 +0800

    reallocate constant from XT to XL, adjust initial value

--- msl_indept/msl_llk_doit.jl
+++ msl_indept/msl_llk_doit.jl
@@ -1,5 +1,5 @@
 # set JULIA_NUM_THREADS=8
-using FileIO, DataFrames, CSV, Statistics, StatsBase, LinearAlgebra, GLM
+using FileIO, DataFrames, CSV, StatsFuns, Statistics, StatsBase, LinearAlgebra, GLM
 DTDIR = "E:/NutStore/Research/mig_leftbh_enrollment"
 WKDIR = "E:/Dropbox/GitHub/Migration_Left_Behind_BLP/msl_indept"
 # WKDIR = "/Users/probmetrics/Dropbox/GitHub/Migration_Left_Behind_BLP/msl_indept"
@@ -12,7 +12,6 @@ include("$WKDIR/msl_llk_loop.jl")
 include("$WKDIR/squarem_helper_funs.jl")
 include("$WKDIR/blp_squarem.jl")
 include("$WKDIR/msl_est_iter.jl")
-
 
 ##
 ## 1. load choice data from MPS
@@ -31,16 +30,17 @@ XMnames = [:lndist, :cross_prov, :cross_regn, :lndist_crsprov, :lndist_crsregn,
            :amenity_pca_flowdur, :migdist_flowdur, :amenity_pca_highsch_f,
            :migdist_highsch_f, :amenity_pca_highsch_m, :migdist_highsch_m,
            :amenity_pca_age_f, :migdist_age_f, :amenity_pca_age_m, :migdist_age_m]
-XTnames = [:highsch_f, :highsch_m, :age_f, :han]
+XTnames = [:highsch_f, :highsch_m, :age_f, :age_m, :han]
 XFnames = [:htreat, :migscore_fcvx_city, :lnhprice, :migscore_treat, :lnhp_treat,
 		   :nchild_lnhp]
-XLnames = [:cfemale, :nchild, :cagey, :cageysq, :age_m]
+XLnames = [:cfemale, :nchild, :cagey, :cageysq]
 XQnames = [:cfemale, :cagey, :nchild, :highsch_f, :highsch_m, :age_f]
 XQJnames = [:tstu2_ratio, :sschool_per]
 
 lnDataShare, Delta_init, lnW, lnP, XQJ_mig, wgt, sgwgt,
 XT, XM, XL, XF, XQ, nalt, nind, dgvec = data_prepare(LeftbhData, XQJnames,
 			 XTnames, XLnames, XFnames, XMnames, XQnames; trs = true)
+XFW = vcat(XF, lnW')
 
 ##
 ## 2. get random draw
@@ -51,7 +51,6 @@ MigBootData = CSV.read("$DTDIR/mig_leftbh_indboot.csv")
 # --- bootstrap random shock ---
 nsim = 20
 ndraw = nind * nsim
-alpha = 0.12
 
 # bootstrap observed preference vars.
 USHK = dropdims(draw_shock(ndraw; dims = 1); dims = 2) # draw iid standard normal random shock
@@ -81,41 +80,38 @@ XbQ = XQ' * [lnq_init[1]; lnq_init[4:end-5]]
 initset = load("$DTDIR/msl_indept_results/msl_indept_est_20190807.jld2")
 initpar = initset["coefx"]
 initdel = initset["deltas"]
+
 xm_init = initpar[11:25]
-xf_init = [initpar[26:31]; initpar[33]; -0.4]
+# xf_init = [initpar[26:31]; initpar[33]; -0.4]
+nparm = size(XT, 1) + size(XL, 1) + size(XM, 1) + size(XF, 1) + 2
 
-nparm = size(XT, 1) + size(XL, 1) + size(XM, 1) + size(XF, 1)  +
-		2*size(XQJ_mig, 1) + 1
+# xt_init = [3.0; lft_init[2:size(XT, 1)]]
+xt_init = lft_init[2:(size(XT, 1) + 1)]
+xl_init = lft_init[(size(XT, 1) + size(XF, 1) + 1):(size(XT, 1) + size(XF, 1) + size(XL, 1) - 1)]
+xf_init = [0.0; lft_init[(size(XT, 1) + 2):(size(XT, 1) + size(XF, 1))]; -0.2]
+# xqj_init = lft_init[(end-size(XQJ_mig, 1)):end-1]
+# bw = 0.194
+# blft = -0.148
+# bitr = -0.167
 
-xt_init = [3.0; lft_init[2:size(XT, 1)]]
-xl_init = lft_init[(size(XT, 1) + size(XF, 1) - 1):(size(XT, 1) + size(XF, 1) + size(XL, 1) - 2)]
-xqj_init = lft_init[(end-size(XQJ_mig, 1)):end-1]
+initval = [xt_init; 4.2; -xl_init; xm_init; xf_init; -1.2]
 
-bw = 0.194
-blft = -0.148
-bitr = -0.167
-
-initval = [xt_init; xl_init; xm_init; xf_init; zeros(length(xqj_init)); xqj_init; -1.0]
-
-parm_names = [:XT_cons; Symbol.("XT_" .* string.(XTnames)); Symbol.("XL_" .* string.(XLnames));
+parm_names = [Symbol.("XT_" .* string.(XTnames)); :XL_cons; Symbol.("XL_" .* string.(XLnames));
 			  Symbol.("XM_" .* string.(XMnames)); :XF_cons; Symbol.("XF_" .* string.(XFnames));
-			  :XF_lnW; Symbol.("XQJ_Mig_" .* string.(XQJnames));
-			  Symbol.("XQJ_Dif_" .* string.(XQJnames)); :lnsigu]
+			  :XF_lnW; :lnsigu]
 
 # --- iterative maximization ---
 ret_msl = msl_est_iter(initval, lnDataShare, initdel, YL, YM, lnW, lnP, XbQ,
-			 			XQJ_mig, XT, XL, XM, XF, USHK, wgt, sgwgt, nind,
-			 			nalt, nsim, dgvec; biter = 1)
+			 			XQJ_mig, XT, XL, XM, XFW, USHK, wgt, sgwgt, nind,
+			 			nalt, nsim, dgvec; biter = 3)
 
 # --- evaluate log-likelihood ---
 @time mig_leftbh_llk(initval, initdel, YL, YM, lnW, lnP, XbQ, XQJ_mig,
-			 		 XT, XL, XM, XF, USHK, wgt, nind, nalt, nsim, dgvec,
-					 0.12, 1)
+			 		 XT, XL, XM, XFW, USHK, wgt, nind, nalt, nsim, dgvec, 1)
 # 444022.5091; 5.1s
 
 @time mig_leftbh_llk_thread(initval, initdel, YL, YM, lnW, lnP, XbQ, XQJ_mig,
-			 		 		XT, XL, XM, XF, USHK, wgt, nind, nalt, nsim, dgvec,
-							0.12, 1)
+			 		 		XT, XL, XM, XFW, USHK, wgt, nind, nalt, nsim, dgvec, 1)
 # 444022.5091; 0.9s for 8 threads
 #
 # --- test for ForwardDiff ---
